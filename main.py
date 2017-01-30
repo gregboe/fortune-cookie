65feda9c447e44d73c2058c70a661986c06785f0
Greg Stead <gregstead@gmail.com>
2017-01-30 08:58:10 -0600

    Modified main handler class, built getRandomFortune function

--- main.py
+++ main.py
@@ -15,11 +15,42 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    #make list of possible fortunes
+    fortunes = [
+        "Today it's up to you to create the peacefulness you long for.",
+        "A friend asks only for your time not your money.",
+        "If you refuse to accept anything but the best, you very often get it.",
+        "A smile is your passport into the hearts of others."
+    ]
+
+    #randomly select one of the fortunes
+    index = random.randint(0,3)
+    #return one that is selected
+    return fortunes[index]
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_paragraph  = "<p>" + "Your fortune is: " + fortune + "</p>"
+
+        lucky_number = random.randint(1,1000)
+        lucky_number_sentece = "Your lucky number is: " + "<strong>" + str(lucky_number)+ "</strong>"
+        lucky_number_paragraph = "<p>" + lucky_number_sentece + "</p>"
+
+        cookie_again_button = """<a href="."><button>Another cookie please!</button></a>"""
+
+        content = header + fortune_paragraph + lucky_number_paragraph + cookie_again_button
+
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
 ], debug=True)
